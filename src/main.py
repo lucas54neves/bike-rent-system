2619b59f3e609d2cc89810ff4ae830c207b2e1e2
Lucas Neves <lucas54neves@gmail.com>
2021-08-29 12:25:47 -0300

    feat: add menu

--- src/main.py
+++ src/main.py
@@ -1,34 +1,88 @@
 from models import Store
 
+def operationsMenu():
+    print(f'========== Menu ==========')
+    print(f'[1] Adicionar bicicleta')
+    print(f'[2] Adicionar cliente')
+    print(f'[3] Exibir biciletas')
+    print(f'[4] Adicionar aluguel')
+    print(f'[5] Calcular aluguel')
+    print(f'[0] Sair')
+    print(f'==========================')
+
 def main():
-    storeOfBikes = Store('A Loja', 'Rua 1')
-    storeOfBikes.addBike('Vermelho')
-    storeOfBikes.addBike('Vermelho')
-    storeOfBikes.addBike('Vermelho')
-    storeOfBikes.addBike('Vermelho')
-    storeOfBikes.addBike('Vermelho')
-    storeOfBikes.addBike('Branco')
-    storeOfBikes.addBike('Branco')
-    storeOfBikes.addBike('Branco')
-    storeOfBikes.addBike('Branco')
-    storeOfBikes.addBike('Branco')
-    storeOfBikes.addClient('Nome1', 'email1@mail.com', '11122233344')
-    storeOfBikes.addClient('Nome2', 'email2@mail.com', '12345678901')
-    # storeOfBikes.addClient('Nome3', 'email3@mail.com', '13345678901')
-    storeOfBikes.addClient('Nome3', 'email3@mail.com', '13345678901')
-    # storeOfBikes.addRental('dailyd', 'email2@mail.com', 1)
-    # storeOfBikes.addRental('weekly', 'email4@mail.com', 1)
-    storeOfBikes.addRental('weekly', 'email3@mail.com', 1)
-    storeOfBikes.addRental('weekly', 'email3@mail.com', 1)
-    storeOfBikes.addRental('weekly', 'email2@mail.com', 3, True)
-    storeOfBikes.addRental('weekly', 'email3@mail.com', 1)
-    storeOfBikes.showBikes()
-    # print(storeOfBikes.bikes)
-    # print(storeOfBikes.rents)
-    # print(storeOfBikes.clients)
-    # storeOfBikes.calculateRental('email1@mail.com')
-    # storeOfBikes.calculateRental('email2@mail.com')
-    storeOfBikes.calculateRental('email3@mail.com')
-    storeOfBikes.showBikes()
+    option = input('Deseja entrar no sistema? [s/n] ')
+
+    if option == 's':
+        store_name = input('Qual o nome da loja? ')
+
+        store_address = input('Qual o endereco da loja? ')
+
+        store = Store(store_name, store_address)
+
+    while option == 's':
+        operationsMenu()
+
+        option_as_string = input('Qual opcao desejada? ')
+        
+        try:
+            option = int(option_as_string)
+        except:
+            print('A entrada deve ser um inteiro entre 0 e 5')
+
+        if option == 1:
+            color = input('Qual a cor? ')
+
+            try:
+                store.addBike(color)
+            except Exception as error:
+                print(str(error))
+        elif option == 2:
+            name = input('Qual o nome do cliente? ')
+
+            email = input('Qual o email do cliente? ')
+            
+            cpf = input('Qual o CPF do cliente? ')
+
+            try:
+                store.addClient(name, email, cpf)
+            except Exception as error:
+                print(str(error))
+        elif option == 3:
+            print(f'=== Estoque de bicicletas ===')
+            
+            store.showBikes()
+        elif option == 4:
+            model = input('Qual modelo do aluguel? ')
+
+            email = input('Qual email do cliente? ')
+
+            quantity = int(input('Qual a quantidade de alugueis? '))
+
+            family = input('O aluguel e de familia? [s/n] ')
+
+            isFamily = family == 's'
+
+            try:
+                store.addRental(model, email, quantity, isFamily)
+            except Exception as error:
+                print(str(error))
+
+        elif option == 5:
+            email = input('Qual email do cliente? ')
+
+            try:
+                value = store.calculateRental(email)
+            except Exception as error:
+                print(str(error))
+
+            print(f'O valor do aluguel e R$ {value}')
+        elif option != 0:
+            print('Opcao nao cadastrada. Tente novamente.')
+
+        if option == 0:
+            option = 'n'
+        else:
+            option = 's'
 
 main()
